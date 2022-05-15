680dc34f5c6b851adc370fc7b5422f6535dd6019
Sry-maker <1961384758@qq.com>
2022-05-15 22:32:23 +0800

    backend

---
+++ backend/src/main/java/com/bibackend/entity/AUTHOR.java
@@ -0,0 +1,50 @@
+package com.bibackend.entity;
+
+
+import lombok.Builder;
+import lombok.Data;
+import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.GeneratedValue;
+import org.neo4j.ogm.annotation.Id;
+import org.neo4j.ogm.annotation.Property;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@NodeEntity(label = "AUTHOR")
+public class AUTHOR implements Serializable{
+    @Id
+    @GeneratedValue
+    private String id;
+
+    @Property
+    private String Hindex;
+
+    @Property
+    private String Pindex;
+
+    @Property
+    private String UPindex;
+
+    @Property
+    private String affiliations;
+
+    @Property
+    private String index;
+
+    @Property
+    private String interests;
+
+    @Property
+    private String name;
+
+    @Property
+    private String publishCount;
+
+    @Property
+    private String referenceCount;
+
+
+
+}
---
+++ backend/src/main/java/com/bibackend/entity/VENUE.java
@@ -0,0 +1,22 @@
+package com.bibackend.entity;
+
+import lombok.Builder;
+import lombok.Data;
+import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.GeneratedValue;
+import org.neo4j.ogm.annotation.Id;
+import org.neo4j.ogm.annotation.Property;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@NodeEntity(label = "VENUE")
+public class VENUE implements Serializable{
+    @Id
+    @GeneratedValue
+    private String id;
+
+    @Property
+    private String name;
+}
