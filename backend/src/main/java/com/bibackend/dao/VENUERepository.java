f878a57173462a99cae23d3390566372550f2b3a
Sry-maker <1961384758@qq.com>
2022-05-17 11:57:35 +0800

    1

--- backend/src/main/java/com/bibackend/dao/VENUERepository.java
+++
@@ -1,7 +0,0 @@
-package com.bibackend.dao;
-
-import com.bibackend.entity.VENUE;
-import org.springframework.data.neo4j.repository.Neo4jRepository;
-
-public interface VENUERepository extends Neo4jRepository<VENUE,Long> {
-}
