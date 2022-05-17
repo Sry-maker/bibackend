d6960578cdbd807c33c43aa0ee123e9afff74679
Sry-maker <1961384758@qq.com>
2022-05-17 11:28:37 +0800

    初步

--- backend/src/main/java/com/bibackend/dao/referRepository.java
+++ backend/src/main/java/com/bibackend/dao/referRepository.java
@@ -3,6 +3,8 @@ package com.bibackend.dao;
 
 import com.bibackend.entity.REFER;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface REFERRepository extends Neo4jRepository<REFER,Long> {
 }
