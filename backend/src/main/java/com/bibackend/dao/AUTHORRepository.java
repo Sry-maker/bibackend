003af0070a978c304d7708215fd7b8a0c354dbde
Sry-maker <1961384758@qq.com>
2022-05-20 20:10:42 +0800

    controller

--- backend/src/main/java/com/bibackend/dao/AUTHORRepository.java
+++ backend/src/main/java/com/bibackend/dao/AUTHORRepository.java
@@ -2,7 +2,13 @@ package com.bibackend.dao;
 
 
 import com.bibackend.entity.AUTHOR;
+import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 
 public interface AUTHORRepository extends Neo4jRepository<AUTHOR,Long> {
+
+//    match(n:AUTHOR)-[r]->(nn) WHERE n.name = 'Chun-Yen Chang' RETURN n,r,nn
+
+
+
 }
